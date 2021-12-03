588823f1ad5eb02dffd754a800eaa0237e1fce2e
David Cumps <david@cumps.be>
2021-12-03 02:23:58 +0100

    Add project files.

---
+++ aleo-peer-finder/NodeState.cs
@@ -0,0 +1,91 @@
+﻿//{
+//    "jsonrpc": "2.0",
+//    "result": {
+//        "candidate_peers": [
+//        "135.181.206.65:4132",
+//        "159.223.124.150:4132",
+//        "144.126.219.193:4132"
+//            ],
+//        "connected_peers": [
+//        "143.198.166.150:4132",
+//        "147.139.105.12:4132",
+//        "46.148.231.72:4132",
+//        "167.99.40.226:4132",
+//        "165.232.145.194:4132",
+//        "161.35.106.91:4132",
+//        "195.201.197.79:4132",
+//        "159.223.118.35:4132",
+//        "188.166.7.13:4132",
+//        "103.244.117.26:4132",
+//        "147.182.213.228:4132",
+//        "37.230.117.126:4132",
+//        "157.245.133.62:4132",
+//        "156.146.34.26:443",
+//        "137.184.192.155:4132",
+//        "137.184.202.162:4132"
+//            ],
+//        "latest_block_height": 8825,
+//        "latest_cumulative_weight": 12653,
+//        "number_of_candidate_peers": 3,
+//        "number_of_connected_peers": 16,
+//        "number_of_connected_sync_nodes": 10,
+//        "software": "snarkOS 2.0.0",
+//        "status": "Syncing",
+//        "type": "Miner",
+//        "version": 11
+//    },
+//    "id": "1"
+//}
+
+using System.Text.Json.Serialization;
+
+namespace AleoPeerFinder
+{
+    internal class NodeState
+    {
+        [JsonPropertyName("jsonrpc")]
+        public string JsonRpc { get; set; }
+
+        [JsonPropertyName("result")]
+        public NodeStateResult Result { get; set; }
+
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+    }
+
+    internal class NodeStateResult
+    {
+        [JsonPropertyName("candidate_peers")]
+        public List<string> CandidatePeers { get; set; }
+
+        [JsonPropertyName("connected_peers")]
+        public List<string> ConnectedPeers { get; set; }
+
+        [JsonPropertyName("latest_block_height")]
+        public int LatestBlockHeight { get; set; }
+
+        [JsonPropertyName("latest_cumulative_weight")]
+        public int LatestCumulativeWeight { get; set; }
+
+        [JsonPropertyName("number_of_candidate_peers")]
+        public int NumberOfCandidatePeers { get; set; }
+
+        [JsonPropertyName("number_of_connected_peers")]
+        public int NumberOfConnectedPeers { get; set; }
+
+        [JsonPropertyName("number_of_connected_sync_nodes")]
+        public int NumberOfConnectedSyncNodes { get; set; }
+
+        [JsonPropertyName("software")]
+        public string Software { get; set; }
+
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("version")]
+        public int Version { get; set; }
+    }
+}
---
+++ aleo-peer-finder/Program.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.Concurrent;
+using System.Net.Http.Json;
+using Microsoft.Net.Http.Headers;
+
+namespace AleoPeerFinder
+{
+    public static class Program
+    {
+        private static readonly HttpClient Client = new();
+
+        private static readonly string RpcBody = "{ \"jsonrpc\": \"2.0\", \"id\": \"1\", \"method\": \"getnodestate\", \"params\": [] }";
+
+        private static readonly List<string> SyncNodes = new()
+        {
+            "144.126.219.193", "165.232.145.194", "143.198.164.241", "188.166.7.13", "167.99.40.226",
+            "159.223.124.150", "137.184.192.155", "147.182.213.228", "137.184.202.162", "159.223.118.35",
+            "161.35.106.91", "157.245.133.62", "143.198.166.150"
+        };
+
+        private static readonly ConcurrentDictionary<string, NodeDetails> Nodes = new();
+
+        private static int _fetchCount;
+
+        public static async Task Main()
+        {
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Add(HeaderNames.UserAgent, "Aleo Peer Finder");
+
+            Console.WriteLine($"Processing {SyncNodes.Count} sync nodes.");
+            await ProcessNodes(SyncNodes);
+
+            var numberOfLoops = 0;
+            while (numberOfLoops < 2 && Nodes.Values.Any(x => x.Block == null))
+            {
+                var newNodes = Nodes
+                    .Where(x => x.Value.Block == null)
+                    .Select(x => x.Key)
+                    .ToList();
+
+                Console.WriteLine($"Processing {newNodes.Count} new nodes.");
+
+                await ProcessNodes(newNodes);
+                numberOfLoops++;
+            }
+
+            var nodes = Nodes
+                .Where(x => x.Value.Block != null)
+                .OrderByDescending(x => x.Value.Weight)
+                .ToList();
+
+            Console.WriteLine();
+            Console.WriteLine($"Found {nodes.Count} nodes.");
+            Console.WriteLine();
+
+            foreach (var (_, node) in nodes)
+                Console.WriteLine($"{node.Ip,22} - {node.Block,5} / {node.Weight,5}");
+
+            Console.WriteLine("Paste this in environment/mod.rs");
+            Console.WriteLine();
+
+            var highestNodes = string.Join(", ", nodes.Take(30).Select(x => $"\"{x.Value.Ip}\""));
+
+            Console.WriteLine("const SYNC_NODES: [&'static str; 30] = [");
+            Console.WriteLine(highestNodes);
+            Console.WriteLine("];");
+        }
+
+        private static async Task ProcessNodes(IEnumerable<string> nodes)
+        {
+            await Parallel.ForEachAsync(nodes, async (node, ct) =>
+            {
+                var fetchCount = Interlocked.Increment(ref _fetchCount);
+
+                try
+                {
+
+                    Console.WriteLine($"#{fetchCount,3} Fetching info for {node}");
+
+                    var rpcCall = await Client.PostAsync($"http://{node}:3032", new StringContent(RpcBody), ct);
+                    var rpcResponse = await rpcCall.Content.ReadFromJsonAsync<NodeState>(cancellationToken: ct);
+
+                    if (rpcResponse != null)
+                        ProcessNode(node, rpcResponse.Result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"#{fetchCount,3} Fetching info for {node} failed: {e.Message}");
+                }
+            });
+        }
+
+        private static void ProcessNode(string node, NodeStateResult result)
+        {
+            var currentHigh = Nodes.Max(x => x.Value.Weight);
+            if (result.LatestCumulativeWeight > currentHigh)
+                Console.WriteLine($"New highest weight found! {result.LatestCumulativeWeight} for {node}");
+
+            Nodes.AddOrUpdate(
+                node,
+                new NodeDetails(node, result.LatestBlockHeight, result.LatestCumulativeWeight),
+                (_, value) =>
+                {
+                    value.Block = result.LatestBlockHeight;
+                    value.Weight = result.LatestCumulativeWeight;
+                    return value;
+                });
+
+            ProcessPeers(result.CandidatePeers);
+            ProcessPeers(result.ConnectedPeers);
+        }
+
+        private static void ProcessPeers(IEnumerable<string> peers)
+        {
+            foreach (var peer in peers)
+                Nodes.TryAdd(peer.Split(":")[0], new NodeDetails(peer));
+        }
+    }
+
+    public class NodeDetails
+    {
+        public string Ip { get; }
+        public int? Block { get; set; }
+        public int? Weight { get; set; }
+
+        public NodeDetails(string ip)
+        {
+            Ip = ip;
+        }
+
+        public NodeDetails(string ip, int block, int weight) : this(ip)
+        {
+            Block = block;
+            Weight = weight;
+        }
+    }
+}
