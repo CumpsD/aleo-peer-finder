2262807549939de5a46a9a5303473abf3eb5ce2c
David Cumps <david@cumps.be>
2021-12-03 02:45:09 +0100

    refactor: split off node details

---
+++ aleo-peer-finder/NodeDetails.cs
@@ -0,0 +1,20 @@
+﻿namespace AleoPeerFinder
+{
+    internal class NodeDetails
+    {
+        public string Ip { get; }
+        public int? Block { get; set; }
+        public int? Weight { get; set; }
+
+        public NodeDetails(string ip)
+        {
+            Ip = ip;
+        }
+
+        public NodeDetails(string ip, int block, int weight) : this(ip)
+        {
+            Block = block;
+            Weight = weight;
+        }
+    }
+}
--- aleo-peer-finder/Program.cs
+++ aleo-peer-finder/Program.cs
@@ -131,22 +131,4 @@ namespace AleoPeerFinder
                 Nodes.TryAdd(peer.Split(":")[0], new NodeDetails(peer));
         }
     }
-
-    public class NodeDetails
-    {
-        public string Ip { get; }
-        public int? Block { get; set; }
-        public int? Weight { get; set; }
-
-        public NodeDetails(string ip)
-        {
-            Ip = ip;
-        }
-
-        public NodeDetails(string ip, int block, int weight) : this(ip)
-        {
-            Block = block;
-            Weight = weight;
-        }
-    }
 }
